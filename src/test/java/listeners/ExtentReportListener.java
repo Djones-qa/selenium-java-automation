14b445645471ea2a90e2368e77eca80cfcbb2fb8
Djones-qa <your@email.com>
2026-03-31 22:08:12 -0400

    Add ExtentReports listener and dependency

---
+++ src/test/java/listeners/ExtentReportListener.java
@@ -0,0 +1,62 @@
+package listeners;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtentReportListener implements ITestListener {
+
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("reports/ExtentReport.html");
+        reporter.config().setTheme(Theme.DARK);
+        reporter.config().setDocumentTitle("Selenium Java Automation Report");
+        reporter.config().setReportName("SauceDemo Test Results");
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Darrius Jones");
+        extent.setSystemInfo("Environment", "SauceDemo");
+        extent.setSystemInfo("Browser", "Chrome Headless");
+        extent.setSystemInfo("OS", System.getProperty("os.name"));
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        extentTest.info("Test started: " + result.getMethod().getDescription());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().log(Status.PASS, "Test passed: " + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().log(Status.FAIL, "Test failed: " + result.getThrowable().getMessage());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().log(Status.SKIP, "Test skipped: " + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+        System.out.println("ExtentReport generated: reports/ExtentReport.html");
+    }
+
+    public static ExtentTest getTest() {
+        return test.get();
+    }
+}
